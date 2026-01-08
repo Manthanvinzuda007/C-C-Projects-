1ccdf89a62479c9cffc7dabee6d0e38f6863449a
Manthan Vinzuda <206708551+Manthanvinzuda007@users.noreply.github.com>
2026-01-08 18:17:16 +0530

    Update Pattern.c

--- Pattern.c
+++ Pattern.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, rows;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    printf("\n");
+    // Outer loop row mate
+    for (i = 1; i <= rows; ++i) {
+        // Inner loop star print karva mate
+        for (j = 1; j <= i; ++j) {
+            printf("* ");
+        }
+        printf("\n"); // Navi line ma java mate
+    }
+    return 0;
+}
 
