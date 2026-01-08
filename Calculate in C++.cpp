0f21867abfd7679fdde25964c793c10a8bb47bff
Manthan Vinzuda <206708551+Manthanvinzuda007@users.noreply.github.com>
2026-01-08 16:04:10 +0530

    Update Calculate in C++.cpp

--- Calculate in C++.cpp
+++ Calculate in C++.cpp
@@ -1,1 +1,27 @@
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    double num1, num2;
+
+    
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    
+    cout << "\n--- Result ---" << endl;
+    cout << "Addition: " << num1 + num2 << endl;
+    cout << "Subtraction: " << num1 - num2 << endl;
+    cout << "Multiplication: " << num1 * num2 << endl;
+
+    if(num2 != 0) {
+        cout << "Division: " << num1 / num2 << endl;
+    } else {
+        cout << "Division: Not possible (Cannot divide by zero)" << endl;
+    }
+
+    return 0;
+}
