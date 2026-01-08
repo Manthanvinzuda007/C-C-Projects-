4d179b5af3637813cf0df2cf497c7a619babba98
Manthan Vinzuda <206708551+Manthanvinzuda007@users.noreply.github.com>
2026-01-08 18:26:09 +0530

    Update Pattern 2.c

--- Pattern 2.c
+++ Pattern 2.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int i, space, k, rows;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    printf("\n");
+    // Row mate loop
+    for (i = 1; i <= rows; i++) {
+        
+        // Space print karva mate loop
+        for (space = 1; space <= rows - i; space++) {
+            printf("  "); // 2 space muki chhe jethi output saru lage
+        }
+
+        // Star print karva mate loop
+        for (k = 1; k <= (2 * i - 1); k++) {
+            printf("* ");
+        }
+
+        printf("\n");
+    }
+    return 0;
+}
 
